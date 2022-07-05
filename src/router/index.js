2f1afbc017c9313e4f10f41d8c967d7e225d938e
Guolei <1789076673@qq.com>
2022-07-05 16:03:54 +0800

    layout页面布局完成

--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,21 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/login',
     component: () => import('../views/login/index.vue')
+  },
+  {
+    path: '/layout',
+    component: () => import('../layout/index.vue'),
+    children: [
+      { path: 'home', component: () => import('../views/home/index.vue') },
+      { path: 'ask', component: () => import('../views/home/index.vue') },
+      { path: 'video', component: () => import('../views/home/index.vue') },
+      { path: 'mine', component: () => import('../views/home/index.vue') }
+    ]
   }
 ]
 
